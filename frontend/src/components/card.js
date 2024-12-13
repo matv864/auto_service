a750aba561356791d34c94a8e33ec63a02208206
Yura1310 <103391854+Yura1310@users.noreply.github.com>
2024-12-14 04:16:33 +1000

    main-page OK1

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -3,14 +3,14 @@
 function Card() {
     return (
         <div class="ml-8 mr-8 shadow-xl bg-black rounded-3xl flex justify-beetwen items-center p-4">
-            <div class="h-full w-[40%] p-4">
+            <div class="h-full w-[40%] sm:p-4">
                 <div class="h-full w-full rounded-xl bg-white">
                     <img src="/images/image 2.png" class="rounded-xl w-full h-full"></img>
                 </div>
             </div>
-            <div class="h-full w-full pl-4 pt-2">
-                <p class="text-white text-5xl">Header</p>
-                <p class="text-white text-xl">texttexttextetetxtetedett</p>
+            <div class="h-full w-full pl-[4px]  sm:pl-4 sm:pt-2">
+                <p class="text-white text-xl sm:text-5xl">Header</p>
+                <p class="text-white text-[15px] sm:text-xl">texttexttextetetxtetedett</p>
             </div>
         </div>
     );
