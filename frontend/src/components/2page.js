96eac7a2b16c8fdd0f618a6806af3d369605a1b8
Eazyan <it@eazyan.ru>
2024-12-14 03:16:42 +1000

    Logika 0.0.1 Pre-Alpha

--- frontend/src/components/2page.js
+++ frontend/src/components/2page.js
@@ -7,7 +7,7 @@ function Second() {
     // Функция для загрузки данных с API
     const fetchPosts = async (channelId) => {
         try {
-            const response = await fetch(`https://auto.love-this-domen.ru/posts?channel_id=${channelId}`, {
+            const response = await fetch(`https://backend.auto.love-this-domen.ru/posts?channel_id=${channelId}`, {
                 headers: { "accept": "application/json" },
             });
             const data = await response.json();
@@ -58,35 +58,7 @@ function Second() {
                     />
                 ))}
             </div>
-<<<<<<< HEAD
         </main>
-=======
-            <div class="p-8 flex justify-center">
-                <button class="w-[200px] h-[100px] shadow-xl rounded-3xl bg-black text-white text-bold text-4xl">Отзывы</button>
-            </div>
-        </div>
-
-        <div class="pl-10 pr-10 h-[70%] grid grid-cols-1 grid-rows-5 lg:grid-cols-2">
-            <Card/>
-            <Card/>
-        </div>
-
-        <div class="h-[120px] w-full flex justify-center">
-            <div class="h-full w-[40%] p-8 flex justify-center">
-                <div class="h-full w-[40%] bg-black rounded-3xl flex justify-center items-center">
-                    <div class="text-white font-bold text-center text-xl"> Посмотреть еще...</div>
-                </div>
-            </div>
-        </div>
-
-        <div class="pl-40 font-bold text-5xl h-full">
-            <p class="">О Компании</p>
-        </div>
-
-
-    </main>
-
->>>>>>> origin/main
     );
 }
 
--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,16 +1,9 @@
 function Card({ title, text, image }) {
     return (
-<<<<<<< HEAD
         <div className="m-4 shadow-xl bg-black rounded-3xl flex justify-between items-center p-4">
             <div className="h-full w-[40%] p-4">
                 <div className="h-full w-full rounded-xl bg-white">
                     <img src={image} alt="" className="rounded-xl w-full h-full"></img>
-=======
-        <div class="ml-8 mr-8 shadow-xl bg-black rounded-3xl flex justify-beetwen items-center p-4">
-            <div class="h-full w-[40%] p-4">
-                <div class="h-full w-full rounded-xl bg-white">
-                    <img src="/images/image 2.png" class="rounded-xl w-full h-full"></img>
->>>>>>> origin/main
                 </div>
             </div>
             <div className="h-full w-full pl-4 pt-2">
