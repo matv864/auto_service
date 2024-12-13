1b0d181e7af66926fcff7e85ac981f0f2ca62926
Yura1310 <103391854+Yura1310@users.noreply.github.com>
2024-12-14 05:17:35 +1000

    ServicePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import MainPage from "./components/mainPage";
+import Service from "./components/Service";
 
 import { BrowserRouter, Routes, Route } from "react-router";
 
@@ -8,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<MainPage />} />
-        <Route path="education" element={<MainPage />} />
-        <Route path="service" element={<MainPage />} />
+        <Route path="education" element={<Service />} />
+        <Route path="service" element={<Service />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Service.js
@@ -0,0 +1,15 @@
+import Header from "./header";
+
+// import Footer from "./footer";
+
+
+function Service() {
+  return (
+    <div className="App" class="flex flex-col min-h-screen">
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default Service;
