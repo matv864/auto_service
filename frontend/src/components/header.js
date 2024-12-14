990f56f4a0f3c9239197ec03d06480fb64f9def0
Yura1310 <103391854+Yura1310@users.noreply.github.com>
2024-12-14 10:30:44 +1000

    about?

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -7,7 +7,7 @@ function Header() {
               <img src="/images/logo.png"></img>
             </div>
             <text class="ml-20 grid grid-cols-4 gap-10 sm:gap-20 lg:gap-40 xl:gap-60 hidden md:flex sm:text-sm lg:text-lg"> 
-              <a href="#about" class="hover:underline">О компании</a>
+              <a href="/#about" class="hover:underline">О компании</a>
               <a href="#" class="hover:underline">Услуги</a>
               <a href="#" class="hover:underline">Проекты</a>
               <a href="#" class="hover:underline">Обучение</a>
